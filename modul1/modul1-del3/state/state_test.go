f776acf229f25fbba3606147f79f78fe6bd36719
henrikraakil <lhraakil@uia.no>
2022-02-08 22:56:08 +0100

    .gitignore

--- modul1/modul1-del3/state/state_test.go
+++ modul1/modul1-del3/state/state_test.go
@@ -8,8 +8,8 @@ func TestViewState(t *testing.T) {
     wanted := "[V| kylling rev korn HS\\________________/ __________________ |Ø]"
 	got := ViewState()
     if got == wanted {
-         fmt.Printf("Feil %s, ønsket %s.", got, wanted)
+         fmt.Printf("Suksess. Fikk %s, ønsket %s.", got, wanted)
     } else {
-        t.Errorf("Fikk %s, ønsket %s.", got, wanted)
+        t.Errorf("Feil. Fikk %s, ønsket %s.", got, wanted)
     }
 }
