38c2d28e10da9b4ee902b1f720480cf046ac0ab9
henrikraakil <lhraakil@uia.no>
2022-03-28 13:31:21 +0200

    update

--- modul3/Listener/dial.go
+++ modul3/Listener/dial.go
@@ -4,14 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-	)
+)
 
 func main() {
-conn, err := net.Dial("tcp", "127.0.0.1:8080")
-if err != nil {
-fmt.Println(err)
-}
-fmt.Fprintf(conn, "Hei")
-echo, err := bufio.NewReader(conn).ReadString('\n')
-fmt.Println(echo)
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer conn.Close()
+	fmt.Fprintf(conn, "Hei\n")
+	echo, err := bufio.NewReader(conn).ReadString('\n')
+	fmt.Println(echo)
 }
